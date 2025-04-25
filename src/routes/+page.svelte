71cacc4b54f3ce806906f8298a7fe9025d1c96f2
hayshin <hayshinbj@gmail.com>
2025-04-25 19:59:12 +0500

    added canvas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import PixelCanvas from "./PixelCanvas.svelte";
+  let color = '#ff0000';
+  let size = 1;
+</script>
+
+<input type="color" bind:value={color} />
+<input type="number" bind:value={size} min="1" max="10" />
+
+<PixelCanvas width={64} height={64} bind:brushColor={color} bind:brushSize={size} />
