54292fded4ecd00d0b3ba5baef347c5bf2b45267
hayshin <hayshinbj@gmail.com>
2025-04-27 12:29:49 +0500

    something works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
-<script>
-  import PixelCanvas from "./PixelCanvas.svelte";
-  let color = '#ff0000';
-  let size = 1;
+<script lang="ts">
+	import { treaty } from '@elysiajs/eden';
+	import type { App } from './api/+server';
+	let a = 0;
+	let b = 0;
+	let total = 0;
+	const app = treaty<App>('localhost:5173');
+
+
+	async function add() {
+		const{ data, status } = await app.api.add.post({ a, b });
+		if (data && status == 200) total = data.total;
+	}
+
 </script>
 
-<input type="color" bind:value={color} />
-<input type="number" bind:value={size} min="1" max="10" />
+<input type="number" bind:value={a} /> +
+<input type="number" bind:value={b} /> =
+{total}
 
-<PixelCanvas width={64} height={64} bind:brushColor={color} bind:brushSize={size} />
+<button onclick={add}>Calculate</button>
