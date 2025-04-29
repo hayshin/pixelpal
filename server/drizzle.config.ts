1f1bab181da603d6f525a741cc76d3887a2b0425
hayshin <hayshinbj@gmail.com>
2025-04-29 23:05:27 +0500

    .env.example

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -5,7 +5,4 @@ export default defineConfig({
   dialect: 'sqlite', // 'mysql' | 'sqlite' | 'turso'
   schema: './src/db/schema.ts',
   // driver: "better-sqlite",
-  dbCredentials: {
-    url: "file:./sqlite.db"
-  }
 })
--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -3,7 +3,7 @@ import { Database } from "bun:sqlite";
 import { sql } from "drizzle-orm";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
-const sqlite = new Database("sqlite.db");
+const sqlite = new Database(process.env.DATABASE_URL ?? "sqlite.db");
 // export const db = drizzle(sqlite, { casing: "snake_case" });
 export const db = drizzle(sqlite);
 migrate(db, { migrationsFolder: './drizzle' });
